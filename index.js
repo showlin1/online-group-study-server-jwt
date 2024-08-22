c145a8007a3222f82f6571847297c54f33a32919
unknown <showlinakter77@gmail.com>
2024-08-22 13:22:50 +0600

    jwt and cookieparser complete

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 9000
 
 const app = express()
-
+// middleware
 const corsOptions = {
     origin: ['http://localhost:5173'],
     credentials: true,
     optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // QuzF4C4rpMRSBpCR
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0dpphli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,15 +28,60 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+// middlewares
+const logger = (req, res, next) => {
+    console.log('log:info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware', token);
+    // no token available
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         const assignmentCollection = client.db('onlineGroupStudy').collection('assignments')
         const myAssignmentCollection = client.db('onlineGroupStudy').collection('myAssignments')
+        const giveMarkCollection = client.db('onlineGroupStudy').collection('giveMark')
+        // auth related api
+        // jwt generate
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d',
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production' ? true : false,
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            })
+                .send({ success: true })
+        })
 
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+        // services related api
         // get all assignment data from db
-        app.get('/assignments', async (req, res) => {
+        app.get('/assignments', logger, verifyToken, async (req, res) => {
             const result = await assignmentCollection.find().toArray()
             res.send(result)
         })
@@ -95,18 +143,55 @@ async function run() {
             res.send(result);
         })
 
-        // get all assignments posted by a specific user
-        app.get('/myAssignments/:email', async (req,res) =>{
+        // get all myAssignments for a user by email from db
+        app.get('/myAssignments/:email', logger, verifyToken, async (req, res) => {
             const email = req.params.email
-            const query = {'othersUser.email':email}
+            const query = { email: email }
             const result = await myAssignmentCollection.find(query).toArray()
             res.send(result)
         })
-        // delete a assignment data from db
-        app.delete('/myAssignment/:id', async (req,res) =>{
+        // get all myAssignments requests from db for assignment owner
+        app.get('/assignmentRequest/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            const query = { 'othersUser.email': email }
+            const result = await myAssignmentCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        // Update my assignment status
+        app.patch('/myAssignment/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const status = req.body
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: status,
+            }
+            const result = await myAssignmentCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+
+        // delete a assignment data from db
+        app.delete('/myAssignment/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
             const result = await myAssignmentCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
+        // giveMark
+
+        app.get('/giveMarks', async (req, res) => {
+            const cursor = giveMarkCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/giveMarks', async (req, res) => {
+            const giveMark = req.body
+            const result = await giveMarkCollection.insertOne(giveMark)
             res.send(result)
         })
 
